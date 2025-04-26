ef5b94055f76416bfeb4c98c35a08f94d5c49389
Hassan-Raza-Shaikh <ammarbatman9@gmail.com>
2025-04-26 14:33:50 +0500

    .

--- basic.cpp
+++ basic.cpp
@@ -3,9 +3,9 @@
 #include<fstream>
 using namespace std;
 
-int main()
+void filecreation_opening(string &filename, fstream &ufile, int &level)
 {
-    string filename, name, last4digits;
+    string name, last4digits;
     int ph_number;
     cout << "Enter your name: " << endl;
     getline(cin, name);
@@ -14,9 +14,9 @@ int main()
         c = tolower(c);
     }
 
-    cout << "Enter your phone number: " << endl;
+    cout << "Enter your pin (4 digits) " << endl;
     cin >> ph_number;
-    cin.ignore(); // Clear the input buffer
+    cin.ignore();
 
     last4digits = to_string(ph_number % 10000);
     cout << "Last 4 digits of phone number: " << last4digits << endl;
@@ -24,7 +24,88 @@ int main()
     filename = name + "_" + last4digits + ".txt";
     cout << "Filename: " << filename << endl;
 
-    fstream ufile;
-
     ufile.open(filename, ios::in | ios::out | ios::app);
+}
+
+void instructions()
+{
+    fstream instructions_file;
+    instructions_file.open("instructions.txt", ios::in);
+    if (!instructions_file)
+    {
+        cout << "Error opening instructions file." << endl;
+        return;
+    }
+    string line;
+    while(getline(instructions_file, line))
+    {
+        cout << line << endl;
+    }
+    instructions_file.close();
+}
+
+void check ecopoints(int &ecopoints, int &level)
+{
+    if(ecopoints == 0)
+    {
+        level = 0;
+    }
+    else if(ecopoints >= 100)
+    {
+        level = 1;
+    }
+    else if(ecopoints >= 200)
+    {
+        level = 2;
+    }
+    else if(ecopoints >= 300)
+    {
+        level = 3;
+    }
+    else if(ecopoints >= 400)
+    {
+        level = 4;
+    }
+    else if(ecopoints >= 500)
+    {
+        level = 5;
+    }
+    else if(ecopoints >= 600)
+    {
+        level = 6;
+    }
+    else if(ecopoints >= 700)
+    {
+        level = 7;
+    }
+    else if(ecopoints >= 800)
+    {
+        level = 8;
+    }
+    else if(ecopoints >= 900)
+    {
+        level = 9;
+    }
+    else
+    {
+        level = 10;
+    }
+    
+}
+
+int main()
+{
+    string filename;
+    fstream ufile;
+    int level;
+    int ecopoints=0;
+    filecreation_opening(filename, ufile, level);
+    if(level == 0)
+    {
+        instructions();
+    }
+    ufile.close();
+
+    return 0;
+
 }
